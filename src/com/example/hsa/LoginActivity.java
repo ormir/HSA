d33b6708afd9597acb23176982de58581304c9ff
Andrea1995 <n-boys@live.com>
2015-01-17 00:00:31 +0100

    kam shkru xmliii

--- src/com/example/hsa/LoginActivity.java
+++ src/com/example/hsa/LoginActivity.java
@@ -16,7 +16,7 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
-		// Implementing the EditText from the xml
+		// Implementing the EditText from the xmliiiiiii
 		user = (EditText)findViewById(R.id.inputName);
 		pass = (EditText)findViewById(R.id.inputPass);
 		
